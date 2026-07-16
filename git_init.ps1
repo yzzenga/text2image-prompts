c525362ea37fbd6f6cc6465a4e677e10e9b78338
Your Name <you@example.com>
2026-07-16 17:25:20 +0800

    初始化项目：文生图提示词工坊 Pro

--- git_init.ps1
+++
@@ -1,6 +0,0 @@
-Set-Location "D:\mimo_work\text2image-prompts"
-git init
-git add .
-git commit -m "初始化项目：文生图提示词工坊 Pro"
-git remote add origin git@github.com:yzzenga/text2image-prompts.git
-git push -u origin master
